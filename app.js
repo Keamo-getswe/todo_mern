d90d704e4fa1e4777bd7b1510f2488d4d9e8cdc0
Keamo-getswe <morobikmobi@gmail.com>
2022-06-16 22:53:59 +0200

    Change data storage to persistent and change footer
    Removed references to global array that stored items and created a mongoDB database for todo list items.
    Altered the structure of the list template file to display the name of todo list items instead of objects.
    Inserted default values into the database.
    
    Changed the footer text.

--- app.js
+++ app.js
@@ -3,22 +3,53 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
-
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const defaultItem1 = new Item({
+    name: "Do the dishes"
+});
+
+const defaultItem2 = new Item({
+    name: "Do the laundry"
+});
+
+const defaultItem3 = new Item({
+    name: "Feed the dogs"
+});
+
+const defaultItems = [defaultItem1, defaultItem2, defaultItem3];
+
 app.get("/", (req, res) => {
-
-	const day = date.getDate();
-
-	res.render("list", {listTitle: day, newListItems: items}); //convention is to use {day: day}
+    const day = date.getDate();
+    Item.find({}, (err, foundItems) => {
+	if (foundItems.length === 0) {
+	    Item.insertMany(defaultItems, (err) => {
+    		if (err) {
+		    console.log(err);
+    	    	} else {
+	    	    console.log("Defaults added successfully");
+    	    	}
+	    });
+	    res.redirect("/");
+	} else {
+	    res.render("list", {listTitle: day, newListItems: foundItems});
+	}
+    });
 });
 
 app.post("/", (req, res) => {
