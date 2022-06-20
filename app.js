cddc8ee8e6f1efa21be3d7b53d0737bd5a932758
Keamo-getswe <morobikmobi@gmail.com>
2022-06-20 23:34:28 +0200

    Create list schema, dynamic (and unique) list creation and list item adding.
    Created lists schema with an items foreign key.
    Used routing parameters to create unique lists dynamically.
    Implemented list item adding functionality for all list. Came across a bug in toot page due to passing date title. Fix still in progress.
    to issue matching date string when searching for it

--- app.js
+++ app.js
@@ -34,6 +34,13 @@ const defaultItem3 = new Item({
 
 const defaultItems = [defaultItem1, defaultItem2, defaultItem3];
 
+const listsSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listsSchema);
+
 app.get("/", (req, res) => {
     const day = date.getDate();
     Item.find({}, (err, foundItems) => {
@@ -52,21 +59,61 @@ app.get("/", (req, res) => {
     });
 });
 
+app.get("/:listName", (req, res) => {
+    const customListName = req.params.listName;
+    List.findOne({name: customListName}, (err, list) =>{
+        if (err) {
+	    console.log(err);
+	} else {
+	    if (list !== null) {
+		res.render("list", {listTitle: list.name, newListItems: list.items});
+	    } else {
+        	const list = new List({
+            	    name: customListName,
+	   	    items: defaultItems
+        	});
+        	list.save();
+		res.redirect("/" + customListName);
+	    }
+	}
+    });
+    
+});
+
+
 app.post("/", (req, res) => {
 
-	const item = req.body.newitem;
-	if (req.body.list === "Work") {
-		workItems.push(item);	
-		res.redirect("/work");
+	const itemName = req.body.newitem;
+	const listName = req.body.list;
+
+	const item = new Item({
+	    name: itemName
+	});
+
+	if (listName === date.getDate()) {
+	    item.save();	
+	    res.redirect("/");
 	} else {
-		items.push(item);
-		res.redirect("/");
+	    List.findOne({name: listName}, (err, list) => {
+		console.log(req.body);
+	        list.items.push(item);
+		list.save();
+		res.redirect("/" + listName);
+	    });
 	}
 
 });
 
-app.get("/work", (req, res) => {
-	res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.post("/delete", (req, res) => {
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId, (err) => {
+	if (err) {
+            console.log(err);
+	} else {
+	    console.log("Item successfully deleted");
+	    res.redirect("/")
+	}
+    });
 });
 
 app.get("/about", (req, res) => {
